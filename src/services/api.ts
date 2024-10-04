6bf2fb0ebee5b399634406a003fc040ff0a163f5
banderGlitch <nernaykumar98@gmail.com>
2024-10-04 14:44:37 +0530

    freshcode

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-
-const API_BASE_URL = 'http://localhost:9000/api';
+const API_BASE_URL = "https://40e4-205-254-167-236.ngrok-free.app";
+// const API_BASE_URL = 'http://localhost:9000/api';
 
 export interface User {
     id: string;
