fcc3cf78539b2dd05f112530ca5fe12d9e740531
banderGlitch <nernaykumar98@gmail.com>
2024-12-27 01:05:18 +0530

    change

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use('/api', playerRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT,'0.0.0.0', () => console.log(`Server running on port ${PORT}`));
 
 app.get('/', (req, res) => {
     res.send('Telegram Bot is running');
